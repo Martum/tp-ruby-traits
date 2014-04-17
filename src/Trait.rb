d596693d10f99b7e44bc34177b5c4da199ae7a9c
Martin Montenegro <martin.92m@gmail.com>
2014-04-17 17:09:05 -0300

    Ejemplo de base

--- src/Trait.rb
+++ src/Trait.rb
@@ -1,3 +1,18 @@
 class Trait
+  def self.name(unNombre)
+    puts "El nombre es"
+    puts unNombre
+  end
 
+  def self.method(unNombre, unBloque)
+    puts unNombre
+  end
+
+  def self.define(&unBloque)
+    instance_eval &unBloque
+  end
+end
+
+Trait.define do
+  name :jose
 end
