3229a0b1e37749012514d568cd04bf4fb8e945d5
Martin Montenegro <martin.92m@gmail.com>
2014-04-17 22:38:14 -0300

    Prueba loca

--- src/Trait.rb
+++ src/Trait.rb
@@ -1,19 +1,49 @@
+class ElTrait
+  attr_accessor :metodos
+
+  def initialize
+    @metodos = Hash.new
+  end
+
+  def agregar_metodo(nombre_metodo, &bloque)
+    metodos[nombre_metodo] = bloque
+  end
+end
 class Trait
+
+  @@trait_name = nil
+  @@objTrait = nil
+
   def self.name(unNombre)
-    puts "El nombre del Trait es: #{unNombre}"
+    @@trait_name = unNombre
+    @@objTrait = ElTrait.new
   end
 
   def self.method(unNombre, &unBloque)
-    puts "Un metodo es: #{unNombre}"
+    @@objTrait.agregar_metodo(unNombre, &unBloque)
   end
 
   def self.define(&unBloque)
     instance_eval &unBloque
+    definir_constante
+  end
+
+  def self.definir_constante()
+    Object.const_set(@@trait_name, @@objTrait)
   end
 end
 
+class Object
+  def uses(unTrait)
+    unTrait.metodos.each do |unMetodo, unBloque|
+      self.send(:define_method, unMetodo, unBloque)
+    end
+  end
+end
+
+# Definir el trait
 Trait.define do
-  name :jose
+  name :Jose
 
   method :te_sumo_20 do |unNumerito|
     unNumerito + 20
@@ -23,3 +53,11 @@ Trait.define do
     unNumerito - 10
   end
 end
+
+# Una clase que lo use
+class UnTrait
+  uses Jose
+end
+
+objetito = UnTrait.new
+puts objetito.te_resto_10(5)
---
+++ src/el_trait.rb
@@ -0,0 +1,2 @@
+class ElTrait
+end
