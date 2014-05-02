cb431a952beb4e0da36c50475a6f87eb04e95bae
Martin Montenegro <martin.92m@gmail.com>
2014-05-02 19:27:39 -0300

    Refactor nombre de clase EjecutarAmbosMetodosResolucion El metodo resolver_conflicto ahora es de instancia

--- src/ejecutar_ambos_metodos.rb
+++ src/ejecutar_ambos_metodos.rb
@@ -1,6 +1,6 @@
-class EjecutarAmbosMetodos
+class EjecutarAmbosMetodosResolucion
 
-  def self.resolver_conflicto(old_method, new_method)
+  def resolver_conflicto(old_method, new_method)
     Proc.new do |*campos|
       self.instance_exec(*campos, &old_method)
       self.instance_exec(*campos, &new_method)
